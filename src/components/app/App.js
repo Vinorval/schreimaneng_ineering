054fa5464dabbdd3e2ff774c858eae05cae4b96d
Vinor <vinorval@yandex.ru>
2023-08-29 00:43:25 +0300

    feat: 'add section Promo'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import AppHeader from '../appHeader/AppHeader';
 import Footer from '../footer/Footer';
+import Promo from '../promo/Promo';
 
 function App() {
   return (
     <div className="App">
       <AppHeader></AppHeader>
+      <Promo></Promo>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/promo/Promo.js
@@ -0,0 +1,12 @@
+import './Promo.css';
+
+function Promo() {
+    return (
+        <section className="promo">
+            <h2 className="promo__title">Автоматизируем объекты любой сложности</h2>
+            <span className="promo__subtitle">Мы - ваш надежный партнер  в области автоматизации</span>
+        </section>
+    )
+}
+
+export default Promo;
